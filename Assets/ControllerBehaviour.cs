db341390e2dbb36e03073bbf702345c14676140e
Ben Hirsh <ben@playgen.com>
2016-09-05 11:52:33 +0100

    initial commit

---
+++ Assets/ControllerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerBehaviour : MonoBehaviour
+{
+
+	private StateController _stateController;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		_stateController.Tick(Time.deltaTime);
+	}
+
+}
