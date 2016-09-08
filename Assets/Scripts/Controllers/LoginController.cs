bf13e12195c21404af024eec330013efa04b9986
Ben Hirsh <ben@playgen.com>
2016-09-08 18:30:48 +0100

    Reenabled SUGAR Login

--- Assets/Scripts/Controllers/LoginController.cs
+++ Assets/Scripts/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using GameWork.Interfaces;
+using PlayGen.SUGAR.Client;
+using PlayGen.SUGAR.Contracts;
 
 
 public class LoginController : ILoginable
@@ -12,26 +14,26 @@ public class LoginController : ILoginable
     public void Login(string username, string password)
     {
 
-        LoginSuccessEvent();
-        //var factory = new SUGARClient("http://localhost:62312/");
-        //var loginController = factory.Account;
+        //LoginSuccessEvent();
+        var factory = new SUGARClient("http://localhost:62312/");
+        var loginController = factory.Account;
 
-        //var accountRequest = new AccountRequest()
-        //{
-        //    Name = username,
-        //    AutoLogin = true,
-        //    Password = password
-        //};
+        var accountRequest = new AccountRequest()
+        {
+            Name = username,
+            AutoLogin = true,
+            Password = password
+        };
 
-        //try
-        //{
-        //    var accountResponse = loginController.Login(accountRequest);
-        //    LoginSuccessEvent();
-        //}
-        //catch (Exception ex)
-        //{
-        //    LoginFailedEvent(ex.Message);
-        //}
+        try
+        {
+            var accountResponse = loginController.Login(accountRequest);
+            LoginSuccessEvent();
+        }
+        catch (Exception ex)
+        {
+            LoginFailedEvent(ex.Message);
+        }
 
 
     }
