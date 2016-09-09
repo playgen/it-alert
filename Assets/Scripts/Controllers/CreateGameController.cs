9727bf7939f4376a6efa94d1f3ad7b124acc5cd1
Ben Hirsh <ben@playgen.com>
2016-09-09 17:28:02 +0100

    Photon create and join game functionality working

---
+++ Assets/Scripts/Controllers/CreateGameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PlayGen.ITAlert.Network;
+
+public class CreateGameController
+{
+    private ITAlertClient _client;
+    public event Action CreateGameSuccessEvent;
+    public event Action<string> CreateGameFailedEvent;
+
+    public CreateGameController(ITAlertClient client)
+    {
+        _client = client;
+    }
+
+    public void CreateGame(string gameName, int maxPlayers)
+    {
+        try
+        {
+            _client.CreateRoom(gameName, maxPlayers);
+            CreateGameSuccessEvent();
+        }
+        catch (Exception ex)
+        {
+            CreateGameFailedEvent(ex.Message);
+        }
+    }
+}
---
+++ Assets/Scripts/GameStates/GameListState.cs
@@ -0,0 +1,56 @@
+﻿using GameWork.States;
+
+public class GameListState : SequenceState
+{
+    private GameListController _controller;
+    private GamesListStateInterface _interface;
+
+    public const string stateName = "GameListState";
+
+
+    public GameListState(GamesListStateInterface @interface, GameListController controller)
+    {
+        _interface = @interface;
+        _controller = controller;
+    }
+
+    public override void Initialize()
+    {
+        _interface.Initialize();
+    }
+
+    public override void Terminate()
+    {
+        _interface.Terminate();
+    }
+
+    public override void Enter()
+    {
+        _interface.Enter();
+    }
+
+    public override void Exit()
+    {
+        _interface.Exit();
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override string Name
+    {
+        get { throw new System.NotImplementedException(); }
+    }
+
+    public override void NextState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void PreviousState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
