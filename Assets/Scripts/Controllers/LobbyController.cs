bf3e90b73ad78a55eac4c37b3b7240807b86c588
Ben Hirsh <ben@playgen.com>
2016-09-13 13:21:33 +0100

    Player lobby list - untested.

--- Assets/Scripts/Controllers/LobbyController.cs
+++ Assets/Scripts/Controllers/LobbyController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using PlayGen.ITAlert.Network;
 
 public class LobbyController
@@ -6,10 +8,26 @@ public class LobbyController
     private ITAlertClient _client;
     public event Action ReadySuccessEvent;
     public event Action<string> ReadyFailedEvent;
+    public event Action<LobbyPlayer[]> RefreshSuccessEvent;
 
     public LobbyController(ITAlertClient client)
     {
         _client = client;
+    }
+
+    public void RefreshPlayerList()
+    {
+        var playerReadyStatus = _client.PlayerReadyStatus;
+        var players = _client.ListCurrentRoomPlayers;
+        var lobbyPlayers = new List<LobbyPlayer>();
+        foreach (var photonPlayer in players)
+        {
+            var name = photonPlayer.name;
+            var isReady = playerReadyStatus[photonPlayer.ID];
+            var lobbyPlayer = new LobbyPlayer(name, isReady);
+            lobbyPlayers.Add(lobbyPlayer);
+        }
+        RefreshSuccessEvent(lobbyPlayers.ToArray());
     }
 
     public void ReadyPlayer()
@@ -35,6 +53,18 @@ public class LobbyController
         catch (Exception ex)
         {
             
+        }
+    }
+
+    public struct LobbyPlayer
+    {
+        public string Name;
+        public bool IsReady;
+
+        public LobbyPlayer(string name, bool isReady)
+        {
+            Name = name;
+            IsReady = isReady;
         }
     }
         
