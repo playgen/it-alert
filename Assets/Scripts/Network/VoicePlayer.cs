a233b6e50078db553b5a3a01344c0b9f27823028
Jared <jared@playgen.com>
2016-09-14 14:47:04 +0100

    voice with transmitting status of each player exposed

---
+++ Assets/Scripts/Network/VoicePlayer.cs
@@ -0,0 +1,39 @@
+﻿using PlayGen.ITAlert.Network;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(PhotonVoiceRecorder))]
+[RequireComponent(typeof(PhotonVoiceSpeaker))]
+public class VoicePlayer : MonoBehaviour
+{
+    private PhotonView _photonView;
+    private PhotonVoiceRecorder _photonVoiceRecorder;
+    private PhotonVoiceSpeaker _photonVoiceSpeaker;
+    
+    public bool IsRecording { get; private set; }
+
+    public bool IsOutputting { get; private set; }
+
+    public int OwnerId
+    {
+        get { return _photonView.ownerId; }
+    }
+
+    private void OnEnable()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _photonVoiceRecorder = GetComponent<PhotonVoiceRecorder>();
+        _photonVoiceSpeaker = GetComponent<PhotonVoiceSpeaker>();
+
+        VoiceClient.ReisterVoicePlayer(_photonView.ownerId, this);
+    }
+
+    void Update()
+    {
+        IsRecording = _photonVoiceRecorder != null && _photonVoiceRecorder.IsTransmitting
+            && PhotonVoiceNetwork.ClientState == ExitGames.Client.Photon.LoadBalancing.ClientState.Joined;
+
+        IsOutputting = _photonVoiceSpeaker != null && _photonVoiceSpeaker.IsPlaying
+            && PhotonVoiceNetwork.ClientState == ExitGames.Client.Photon.LoadBalancing.ClientState.Joined;
+    }
+}
