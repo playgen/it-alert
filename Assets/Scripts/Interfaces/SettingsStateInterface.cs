c1385e840927fca6aafa60899764ddbc9f02c5f9
Felix <felix@playgen.com>
2016-09-15 12:08:19 +0100

    add settings screen functionality and faded screenshot to show how it will look when implemented

---
+++ Assets/Scripts/Interfaces/SettingsStateInterface.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using GameWork.Commands.States;
+using GameWork.Interfacing;
+using UnityEngine.UI;
+
+public class SettingsStateInterface : StateInterface
+{
+    private GameObject _settingsPanel;
+
+    public override void Initialize()
+    {
+        // Join Game Popup
+        _settingsPanel = GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer");
+        var panelButtons = new ButtonList("SettingsContainer/SettingsPanelContainer/ButtonPanel");
+
+        var backButton = panelButtons.GetButton("BackButtonContainer");
+        backButton.onClick.AddListener(OnBackClick);
+    }
+
+    private void OnBackClick()
+    {
+        EnqueueCommand(new PreviousStateCommand());
+    }
+
+    public override void Enter()
+    {
+        _settingsPanel.SetActive(true);
+    }
+    public override void Exit()
+    {
+        _settingsPanel.SetActive(false);
+    }
+}
