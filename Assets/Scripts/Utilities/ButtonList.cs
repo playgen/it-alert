f6761aa71cd405e5acc6ffbb90119ab6e8d47a31
Ellis Spice <ellis@playgen.com>
2016-09-15 12:55:43 +0100

    Standard best fit functionality added to ButtonList script

--- Assets/Scripts/Utilities/ButtonList.cs
+++ Assets/Scripts/Utilities/ButtonList.cs
@@ -6,9 +6,27 @@ public class ButtonList
 {
     private GameObject[] _buttons;
 
-    public ButtonList(string menuPath)
+    public ButtonList(string menuPath, bool standardBestFit = false)
     {
         _buttons = GameObjectUtilities.FindAllChildren(menuPath);
+        if (standardBestFit)
+        {
+            int smallestFontSize = 0;
+            foreach (var button in _buttons)
+            {
+                button.GetComponentInChildren<Text>().resizeTextForBestFit = true;
+                button.GetComponentInChildren<Text>().cachedTextGenerator.Populate(button.GetComponentInChildren<Text>().text, button.GetComponentInChildren<Text>().GetGenerationSettings(button.GetComponentInChildren<Text>().rectTransform.rect.size));
+                button.GetComponentInChildren<Text>().resizeTextForBestFit = false;
+                if (button.GetComponentInChildren<Text>().cachedTextGenerator.fontSizeUsedForBestFit < smallestFontSize || smallestFontSize == 0)
+                {
+                    smallestFontSize = button.GetComponentInChildren<Text>().cachedTextGenerator.fontSizeUsedForBestFit;
+                }
+            }
+            foreach (var button in _buttons)
+            {
+                button.GetComponentInChildren<Text>().fontSize = smallestFontSize;
+            }
+        }
     }
 
     public Button GetButton(string containerName)
