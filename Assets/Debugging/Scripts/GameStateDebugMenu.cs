c30545d0e2a5727a05f44fb33af0c407651e2e6b
Jared Glass <jared@playgen.com>
2016-09-15 15:42:17 +0100

    game state debug menu

---
+++ Assets/Debugging/Scripts/GameStateDebugMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using GameWork.States;
+using UnityEngine;
+using PlayGen.ITAlert.GameStates;
+
+namespace Debugging
+{
+    public class GameStateDebugMenu : MonoBehaviour
+    {
+        private ControllerBehaviour _controllerBehaviour;
+        private TickableStateController<TickableSequenceState> _stateController;
+
+        void Start()
+        {
+            _controllerBehaviour = GameObject.FindObjectOfType<ControllerBehaviour>();
+
+            _stateController = (TickableStateController<TickableSequenceState>) GetField(_controllerBehaviour.GetType(),
+                _controllerBehaviour,
+                "_stateController");
+        }
+
+        private void OnGUI()
+        {
+            GameStateButton(LoginState.StateName);
+            GameStateButton(LoadingState.StateName);
+            GameStateButton(MenuState.StateName);
+            GameStateButton(LobbyState.stateName);
+            GameStateButton(GamesListState.StateName);
+            GameStateButton(CreateGameState.StateName);
+            GameStateButton(SettingsState.StateName);
+            GameStateButton(GameState.StateName);
+        }
+
+        private object GetField(Type type, object instance, string fieldName)
+        {
+            var field = type.GetField(fieldName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var value = field.GetValue(instance);
+
+            return value;
+        }
+
+        private void GameStateButton(string stateName)
+        {
+            if (GUILayout.Button(stateName))
+            {
+                _stateController.ChangeState(stateName);
+            }
+        }
+    }
+}
