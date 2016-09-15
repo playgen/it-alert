48bc71ca880510e7e3b566ae8914b71827ca93c1
Jack Coldridge <jack@playgen.com>
2016-09-15 11:28:32 +0100

    merge unity projects

---
+++ Assets/Scripts/Network/Director.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PlayGen.ITAlert.Simulation;
+using PlayGen.ITAlert.Simulation.Contracts;
+using PlayGen.ITAlert.TestData;
+using UnityEngine.UI;
+
+// ReSharper disable CheckNamespace
+
+/// <summary>
+/// There should only ever be one instance of this
+/// </summary>
+public class Director : MonoBehaviour
+{
+	/// <summary>
+	/// Entity to GameObject map
+	/// </summary>
+	private static readonly Dictionary<int, UIEntity> Entities = new Dictionary<int, UIEntity>();
+
+	/// <summary>
+	/// Simulation
+	/// </summary>
+	private static Simulation _simulation;
+
+	/// <summary>
+	/// Current State
+	/// </summary>
+	private static GameState _state;
+
+	//TODO: load this dynamically
+	/// <summary>
+	/// How fast the simulator is running
+	/// </summary>
+	public const float SimulationTick = 0.25f;
+
+	public static float SimulationAnimationRatio;
+
+	/// <summary>
+	/// has the simulation been initialized
+	/// </summary>
+	private static bool _initialized;
+
+	/// <summary>
+	/// the active player
+	/// </summary>
+	private static PlayerBehaviour _player;
+
+	public static PlayerBehaviour Player
+	{
+		get { return _player; }
+	}
+
+	public static System.Random Random = new System.Random((int) DateTime.UtcNow.Ticks);
+
+	private static GameObject _gameOver;
+
+	/// <summary>
+	/// Get entity wrapper by id
+	/// </summary>
+	/// <param name="id"></param>
+	/// <returns></returns>
+	public static UIEntity GetEntity(int id)
+	{
+		UIEntity entity;
+		if (Entities.TryGetValue(id, out entity))
+		{
+			return entity;
+		}
+		throw new Exception(string.Format("Entity id:{0} not found", id));
+	}
+
+	#region Initialization
+
+	private void Start()
+	{
+		SimulationAnimationRatio = Time.deltaTime/SimulationTick;
+		//TODO: replace with code to init from remote state
+		InitializeTestSimulation();
+
+		// center graph 
+		UIConstants.NetworkOffset -= new Vector2((float) _simulation.GraphSize.X/2*UIConstants.SubsystemSpacing.x, (float) _simulation.GraphSize.Y/2*UIConstants.SubsystemSpacing.y);
+
+		SetState();
+		CreateInitialEntities();
+		SelectPlayer();
+
+		_initialized = true;
+		_gameOver = GameObject.Find("Canvas/GameOver");
+		_gameOver.SetActive(false);
+	}
+
+	private void Awake()
+	{
+
+	}
+
+	private void InitializeTestSimulation()
+	{
+		_simulation = ConfigHelper.GenerateSimulation(6, 3, 9, 9, 4);
+	}
+
+	private void SelectPlayer()
+	{
+		var players = Entities.Values.Where(e => e.Type == EntityType.Player).Select(e => e.EntityBehaviour as PlayerBehaviour).ToArray();
+		//TODO: the player id should be passed along with initialization data
+		_player = players[Random.Next(0, players.Length)];
+		GameObject.Find("Canvas/Score").GetComponent<Image>().color = _player.PlayerColor;
+		GameObject.Find("Canvas/Score/Icon").GetComponent<Image>().color = _player.PlayerColor;
+		_player.EnableDecorator();
+	}
+
+	/// <summary>
+	/// Create the entities from the 
+	/// </summary>
+	private static void CreateInitialEntities()
+	{
+		foreach (var stateEntity in _state.Entities)
+		{
+			CreateEntity(stateEntity.Key, stateEntity.Value);
+		}
+		// initialize after the entities have been created as some will need to reference each other
+		foreach (var stateEntity in _state.Entities)
+		{
+			GetEntity(stateEntity.Key).EntityBehaviour.Initialize(stateEntity.Value);
+		}
+	}
+
+	#endregion
+
+	#region Unity Update
+
+	public void FixedUpdate()
+	{
+	}
+
+	#endregion
+
+	#region State Update
+
+
+	private static void SetState()
+	{
+		_state = _simulation.GetState();
+	}
+
+
+	private static void CreateEntity(int id, EntityState state)
+	{
+		var entity = new UIEntity(state);
+		Entities.Add(id, entity);
+	}
+
+	private static void UpdateEntityStates()
+	{
+		if (_state.IsGameFailure)
+		{
+			OnGameOver();
+		}
+		else
+		{
+
+			foreach (var newEntity in _state.Entities.Where(ekvp => Entities.ContainsKey(ekvp.Key) == false))
+			{
+				CreateEntity(newEntity.Key, newEntity.Value);
+				GetEntity(newEntity.Key).EntityBehaviour.Initialize(newEntity.Value);
+			}
+
+			foreach (var stateEntity in _state.Entities)
+			{
+				GetEntity(stateEntity.Key).UpdateEntityState(stateEntity.Value);
+			}
+			// remove dead entities
+			// TODO: make sure nothing is still referencing these
+			foreach (var entityToRemove in Entities.Keys.Except(_state.Entities.Keys).ToArray())
+			{
+				Destroy(GetEntity(entityToRemove).GameObject);
+				Entities.Remove(entityToRemove);
+			}
+		}
+	}
+
+	private static void OnGameOver()
+	{
+		_gameOver.SetActive(true);
+	}
+
+	#endregion
+
+	/// <summary>
+	/// manually advance the simulation tick and propogate changes
+	/// </summary>
+	public static void Tick()
+	{
+		//TODO: replace super lazy way of stopping the game
+		if (_state.IsGameFailure == false)
+		{
+			_simulation.Tick();
+
+			SetState();
+			UpdateEntityStates();
+		}
+	}
+
+	#region UI accessors
+
+	private static void DoInitialized(Action action)
+	{
+		if (_initialized)
+		{
+			action();
+		}
+	}
+
+	private static T GetInitialized<T>(Func<T> func)
+	{
+		if (_initialized)
+		{
+			return func();
+		}
+		return default(T);
+	}
+
+	public static string GetScore()
+	{
+		return GetInitialized(() => _state.Score.ToString());
+	}
+
+	public static string GetTimer()
+	{
+		//TODO: returning the tick is only temporary
+		
+		return GetInitialized(() => _state.CurrentTick.ToString());
+	}
+
+	#endregion
+
+	#region commands (temporary) 
+	
+	//TODO: better implementation
+
+	public static void RequestMovePlayer(int destinationId)
+	{
+		_simulation.RequestMovePlayer(_player.Id, destinationId);
+	}
+
+	public static void RequestActivateItem(int itemId)
+	{
+		_simulation.RequestActivateItem(_player.Id, itemId);
+	}
+
+	public static void RequestDropItem(int itemId)
+	{
+		_simulation.RequestDropItem(_player.Id, itemId);
+	}
+
+	public static void RequestPickupItem(int itemId, int subsystemId)
+	{
+		_simulation.RequestPickupItem(_player.Id, itemId, subsystemId);
+	}
+
+	public static void SpawnVirus()
+	{
+		var subsystems = Entities.Values.Where(e => e.Type == EntityType.Subsystem).ToArray();
+		_simulation.SpawnVirus(subsystems[Random.Next(0, subsystems.Length)].EntityBehaviour.Id);
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/Network/UIManager.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+#pragma warning disable 649
+
+
+// ReSharper disable once CheckNamespace
+// ReSharper disable once InconsistentNaming
+public class UIManager : MonoBehaviour
+{
+	#region editor properties
+
+	/// <summary>
+	/// upgrade counter text component
+	/// </summary>
+	[SerializeField]
+	private Text _upgradeText;
+
+	/// <summary>
+	/// repair counter text component
+	/// </summary>
+	[SerializeField]
+	private Text _repairText;
+
+	/// <summary>
+	/// score counter text component
+	/// </summary>
+	[SerializeField]
+	private Text _scoreText;
+
+	/// <summary>
+	/// timer text component
+	/// </summary>
+	[SerializeField]
+	private Text _timerText;
+
+	/// <summary>
+	/// warning text component
+	/// </summary>
+	[SerializeField]
+	private Text _warningText;
+
+
+	[SerializeField]
+	private GameObject _hintObject;
+
+	//[SerializeField]
+	//private SVGImage _hintObjectIcon;
+
+	#endregion
+
+	//TODO: why are these coroutines?
+	private static Coroutine _warningFlash;
+	private static Coroutine _hintMove;
+	private static int _flashCount;
+
+	private void Awake()
+	{
+
+	}
+	
+	private void FixedUpdate () {
+		//if (_upgradeGen != null)
+		//{
+		//	//_upgradeText.text = _upgradeGen.Amount.ToString();
+		//}
+		//if (_repairGen != null)
+		//{
+		//	//_repairText.text = _repairGen.Amount.ToString();
+		//}
+
+		SetScore();
+		SetTimer();
+	}
+
+	#region read game params
+
+	private void SetScore()
+	{
+		_scoreText.text = Director.GetScore();
+	}
+
+	private void SetTimer()
+	{
+		_timerText.text = Director.GetTimer();
+	}
+
+	#endregion
+
+	#region Warnings
+
+	private IEnumerator StartWarning()
+	{
+		while (_flashCount > 0)
+		{
+			if (_flashCount % 2 == 0)
+			{
+				_warningText.color += new Color(0, 0, 0, Time.fixedDeltaTime*2);
+			}
+			else
+			{
+				_warningText.color -= new Color(0, 0, 0, Time.fixedDeltaTime*2);
+			}
+			if (_warningText.color.a < 0 || _warningText.color.a > 1)
+			{
+				_flashCount--;
+			}
+			yield return new WaitForFixedUpdate();
+		}
+		_warningText.color = new Color(_warningText.color.r, _warningText.color.g, _warningText.color.b, 0);
+		_warningFlash = null;
+	}
+
+
+	public static void DisplayWarning (string warning)
+	{
+		//TODO: apparently this can't be static
+		_flashCount += 2;
+		//_warningText.text = warning;
+		if (_warningFlash == null)
+		{
+			_flashCount += (int)((4 - _flashCount) / 2) * 2;
+			//_warningFlash = StartCoroutine(StartWarning());
+		}
+	}
+
+	//public void DisplayHint(SVGAsset image, string message)
+	//{
+	//	_hintObject.GetComponentInChildren<Text>().text = message.ToUpper();
+	//	_hintObjectIcon.vectorGraphics = image;
+	//	if (_hintMove == null)
+	//	{
+	//		_hintMove = StartCoroutine(MoveHint());
+	//	}
+	//}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	private IEnumerator MoveHint()
+	{
+		Vector2 pos = _hintObject.GetComponent<RectTransform>().anchoredPosition;
+		bool upward = true;
+		while (pos.y >= -75)
+		{
+			if (upward)
+			{
+				pos += new Vector2(0, 2);
+			} else
+			{
+				pos -= new Vector2(0, 2);
+			}
+			if (upward && pos.y >= 75)
+			{
+				upward = false;
+				yield return new WaitForSeconds(3);
+			}
+			_hintObject.GetComponent<RectTransform>().anchoredPosition = pos;
+			yield return new WaitForFixedUpdate();
+		}
+		_hintObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -75);
+		_hintMove = null;
+	}
+	
+	#endregion
+}
+
