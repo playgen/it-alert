22e5ae37eaaf0f946131ab927ed414dc9b199b28
Ben Hirsh <ben@playgen.com>
2016-09-16 18:50:47 +0100

    voice chat icons in lobby, create game reset fields. LobbyState statename variable re capitalisation

--- Assets/Scripts/GameStates/CreateGameState.cs
+++ Assets/Scripts/GameStates/CreateGameState.cs
@@ -66,7 +66,7 @@ public class CreateGameState : TickableSequenceState
 
     public override void NextState()
     {
-        ChangeState(LobbyState.stateName);
+        ChangeState(LobbyState.StateName);
     }
 
     public override void PreviousState()
