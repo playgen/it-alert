8891e6dd5cdf5f54ed09353be4b83a20677f7575
Jared Glass <jared@playgen.com>
2016-09-16 16:40:35 +0100

    quick match will now create or join a  game

---
+++ Assets/Scripts/Controllers/QuickGameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GameWork.Commands.Interfaces;
+using PlayGen.ITAlert.Network;
+
+public class QuickGameController : ICommandAction
+{
+    private readonly ITAlertClient _client;
+    private readonly CreateGameController _createGameController;
+    private readonly int _defaultMaxPlayers;
+
+    private string UniqueGameName
+    {
+        get { return Guid.NewGuid().ToString().Substring(0, 7); }
+    }
+
+    public QuickGameController(ITAlertClient client, CreateGameController createGameController, int defaultMaxPlayers)
+    {
+        _client = client;
+        _createGameController = createGameController;
+        _defaultMaxPlayers = defaultMaxPlayers;
+    }
+
+    public void QuickMatch()
+    {
+        if (0 < _client.ListRooms(ListRoomsFilters.Open).Length)
+        {
+            _client.JoinRandomRoom();
+        }
+        else
+        {
+            _createGameController.CreateGame(UniqueGameName, _defaultMaxPlayers);
+        }
+    }
+}
