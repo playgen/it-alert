7ce6e8faf7fcc07206cd6d7e23e4c29e6529a201
Jack Coldridge <jack@playgen.com>
2016-10-24 11:23:13 +0100

    additional work in progress files

--- Simulation/PlayGen.ITAlert.Simulation.Components/Visitors/VisitorBehaviourComponentBase.cs
+++ Simulation/PlayGen.ITAlert.Simulation.Components/Visitors/VisitorBehaviourComponentBase.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PlayGen.Engine.Components;
+using PlayGen.Engine.Components.Behaviour;
+using PlayGen.Engine.Serialization;
+using PlayGen.ITAlert.Simulation.Entities.Visitors;
+using PlayGen.ITAlert.Simulation.Entities.World;
 
 namespace PlayGen.ITAlert.Simulation.Components.Visitors
 {
-	class VisitorComponentBase
+	public abstract class VisitorBehaviourComponentBase : BehaviourComponentBase, IVisitorComponent
 	{
+		[SyncState(StateLevel.Differential)]
+		private int _enterCurrentNodeTick;
+
+		protected VisitorBehaviourComponentBase(IComponentContainer container) 
+			: base(container)
+		{
+		}
+
+		public virtual void OnEnterNode(INode current)
+		{
+			_enterCurrentNodeTick = 0;
+		}
+
+		public virtual void OnExitNode(INode current)
+		{
+		}
+
+		public virtual void OnTick(INode currentNode)
+		{
+		}
 	}
 }
