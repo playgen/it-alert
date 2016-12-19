d990ead09447a63c11bec76f6c5afd747286710d
Jared <jared@playgen.com>
2016-12-19 20:21:49 +0000

    minor name fixes

--- Server/PlayGen.ITAlert.Photon.Plugins/SUGAR/SUGARConfigurationSection.cs
+++ Server/PlayGen.ITAlert.Photon.Plugins/SUGAR/SUGARConfigurationSection.cs
@@ -4,35 +4,35 @@ namespace PlayGen.ITAlert.Photon.Plugins.SUGAR
 {
     public class SUGARConfigurationSection : ConfigurationSection
     {
-        [ConfigurationProperty("BaseAddress")]
+        [ConfigurationProperty("BaseAddress", IsRequired = true)]
         public string BaseAddress
         {
             get { return (string)this["BaseAddress"]; }
             set { this["BaseAddress"] = value; }
         }
 
-        [ConfigurationProperty("GameId")]
+        [ConfigurationProperty("GameId", IsRequired = true)]
         public int GameId
         {
             get { return (int)this["GameId"]; }
             set { this["GameId"] = value; }
         }
 
-        [ConfigurationProperty("AccountName")]
+        [ConfigurationProperty("AccountName", IsRequired = true)]
         public string AccountName
         {
             get { return (string)this["AccountName"]; }
             set { this["AccountName"] = value; }
         }
 
-        [ConfigurationProperty("AccountPassword")]
+        [ConfigurationProperty("AccountPassword", IsRequired = true)]
         public string AccountPassword
         {
             get { return (string)this["AccountPassword"]; }
             set { this["AccountPassword"] = value; }
         }
 
-        [ConfigurationProperty("AccountSource")]
+        [ConfigurationProperty("AccountSource", IsRequired = true)]
         public string AccountSource
         {
             get { return (string)this["AccountSource"]; }
