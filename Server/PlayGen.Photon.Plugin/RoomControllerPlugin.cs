b7bd92d0a2efdddcc5060b4b1b69e7d2034f2716
Jared <jared@playgen.com>
2017-01-03 15:56:46 +0000

    refactor

--- Server/PlayGen.Photon.Plugin/RoomControllerPlugin.cs
+++ Server/PlayGen.Photon.Plugin/RoomControllerPlugin.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using Photon.Hive.Plugin;
 using PlayGen.Photon.Messaging;
 using PlayGen.Photon.Players;
-using PlayGen.Photon.Players.Extensions;
 using PlayGen.Photon.Plugin.Interfaces;
 using PlayGen.Photon.SUGAR;
-using PlayGen.Photon.Messages;
 using PlayGen.Photon.Messaging.Interfaces;
 
 namespace PlayGen.Photon.Plugin
@@ -18,6 +15,7 @@ namespace PlayGen.Photon.Plugin
 
         private readonly Messenger _messenger;
         private readonly PlayerManager _playerManager = new PlayerManager();
+        private readonly PlayerManagerHandler _playerManagerHandler;
         private readonly Controller _sugarController = new Controller();
         private readonly RoomStateController _stateController;
 
@@ -31,7 +29,7 @@ namespace PlayGen.Photon.Plugin
             _messenger = new Messenger(messageSerializationHandler, this);
             _stateController = roomStateControllerFactory.Create(this, _messenger, _playerManager, _sugarController);
 
-            _messenger.Subscribe((int)Channels.Players, ProcessPlayersMessage);
+            _playerManagerHandler = new PlayerManagerHandler(_playerManager, _messenger);
         }
 
         ~RoomControllerPlugin()
@@ -43,7 +41,7 @@ namespace PlayGen.Photon.Plugin
         {
             if (_isDisposed) return;
 
-            _messenger.Unsubscribe((int)Channels.Players, ProcessPlayersMessage);
+            _playerManagerHandler.Dispose();
 
             _isDisposed = true;
         }
@@ -71,14 +69,14 @@ namespace PlayGen.Photon.Plugin
                 ? info.Request.ActorNr
                 : 1;
 
-            AddAndBroadcastPlayer(playerId);
+            _playerManagerHandler.AddAndBroadcastPlayer(playerId);
             _stateController.OnCreate(info);
             base.OnCreateGame(info);
         }
 
         public override void OnJoin(IJoinGameCallInfo info)
         {
-            AddAndBroadcastPlayer(info.ActorNr);
+            _playerManagerHandler.AddAndBroadcastPlayer(info.ActorNr);
             _stateController.OnJoin(info);
             base.OnJoin(info);
         }
@@ -88,55 +86,6 @@ namespace PlayGen.Photon.Plugin
             _playerManager.Remove(info.ActorNr);
             _stateController.OnLeave(info);
             base.OnLeave(info);
-        }
-
-        private void ProcessPlayersMessage(Message message)
-        {
-            var listPlayersMessage = message as ListPlayersMessage;
-            if (listPlayersMessage != null)
-            {
-                ListPlayers(listPlayersMessage.PhotonId);
-                return;
-            }
-
-            var updatePlayerMessage = message as UpdatePlayerMessage;
-            if (updatePlayerMessage != null)
-            {
-                UpdatePlayer(updatePlayerMessage.Player);
-                return;
-            }
-        }
-
-        private void AddAndBroadcastPlayer(int playerId)
-        {
-            var existingPlayers = _playerManager.PlayersPhotonIds;
-
-            var name = "player" + playerId;
-            var status = PlayerStatus.NotReady;
-            var color = _playerManager.Players.GetUnusedColor();
-
-            _playerManager.Create(playerId, null, name, color, status);
-            _messenger.SendMessage(existingPlayers, ServerPlayerId, new ListedPlayersMessage
-            {
-                Players = _playerManager.Players,
-            });
-        }
-
-        private void UpdatePlayer(Player player)
-        {
-            _playerManager.UpdatePlayer(player);
-            _messenger.SendAllMessage(ServerPlayerId, new ListedPlayersMessage
-            {
-                Players = _playerManager.Players,
-            });
-        }
-
-        private void ListPlayers(int photonId)
-        {
-            _messenger.SendMessage(new List<int>() {photonId}, ServerPlayerId, new ListedPlayersMessage
-            {
-                Players = _playerManager.Players,
-            });
         }
     }
 }
