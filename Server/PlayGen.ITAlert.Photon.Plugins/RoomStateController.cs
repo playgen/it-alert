447a60a1664812666858429550347b65fdad3bb8
Jack Coldridge <jack@playgen.com>
2017-01-04 19:38:54 +0000

    temp update server for new gamewrok - broke refactor - rename solution

--- Server/PlayGen.ITAlert.Photon.Plugins/RoomStateController.cs
+++ Server/PlayGen.ITAlert.Photon.Plugins/RoomStateController.cs
@@ -1,33 +1,37 @@
-﻿using GameWork.Core.States.Controllers;
+﻿using GameWork.Core.States;
 using Photon.Hive.Plugin;
 using PlayGen.ITAlert.Photon.Plugins.RoomStates;
 
 namespace PlayGen.ITAlert.Photon.Plugins
 {
-    public class RoomStateController : StateController<RoomState>
-    {
-        public RoomStateController(params RoomState[] states) : base(states)
-        {
-        }
+	public class RoomStateController : StateController<RoomState>
+	{
+		public RoomStateController(params RoomState[] states) : base(states)
+		{
+		}
 
-        public void OnCreate(ICreateGameCallInfo info)
-        {
-            States[ActiveState].OnCreate(info);
-        }
+		public void OnCreate(ICreateGameCallInfo info)
+		{
+			// TODO: gamework upgrade
+			//States[ActiveState].OnCreate(info);
+		}
 
-        public void OnJoin(IJoinGameCallInfo info)
-        {
-            States[ActiveState].OnJoin(info);
-        }
+		public void OnJoin(IJoinGameCallInfo info)
+		{
+			// TODO: gamework upgrade
+			//States[ActiveState].OnJoin(info);
+		}
 
-        public void OnLeave(ILeaveGameCallInfo info)
-        {
-            States[ActiveState].OnLeave(info);
-        }
+		public void OnLeave(ILeaveGameCallInfo info)
+		{
+			// TODO: gamework upgrade
+			//States[ActiveState].OnLeave(info);
+		}
 
-        public void OnRaiseEvent(IRaiseEventCallInfo info)
-        {
-            States[ActiveState].OnRaiseEvent(info);
-        }
-    }
+		public void OnRaiseEvent(IRaiseEventCallInfo info)
+		{
+			// TODO: gamework upgrade
+			//States[ActiveState].OnRaiseEvent(info);
+		}
+	}
 }
