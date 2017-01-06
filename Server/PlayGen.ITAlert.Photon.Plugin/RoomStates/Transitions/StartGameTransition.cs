081a0e4565b77c3de4e54df14dd9726173c7754d
Jared <jared@playgen.com>
2017-01-06 15:30:02 +0000

    start game server transition

---
+++ Server/PlayGen.ITAlert.Photon.Plugin/RoomStates/Transitions/StartGameTransition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GameWork.Core.States.Commands.Interfaces;
+using GameWork.Core.States.Interfaces;
+
+namespace PlayGen.ITAlert.Photon.Plugin.RoomStates.Transitions
+{
+	public class StartGameTransition : IStateTransition, IDisposable
+	{
+		private LobbyState _lobbyState;
+		private IChangeStateAction _changeStateAction;
+		private bool _isDisposed;
+
+		~StartGameTransition()
+		{
+			Dispose();
+		}
+
+		public void Setup(LobbyState lobbyState, IChangeStateAction changeStateAction)
+		{
+			_changeStateAction = changeStateAction;
+			_lobbyState = lobbyState;
+			_lobbyState.GameStartedEvent += OnGameStarted;
+		}
+
+		public void Dispose()
+		{
+			if (_isDisposed) return;
+
+			_lobbyState.GameStartedEvent -= OnGameStarted;
+
+			_isDisposed = true;
+		}
+
+		private void OnGameStarted()
+		{
+			_changeStateAction.ChangeState(GameState.StateName);
+		}
+	}
+}
