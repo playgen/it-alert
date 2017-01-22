835de898e1260d5ba21baf69fd54ebe87666230e
Jack Coldridge <jack@coldridge.com>
2017-01-22 18:09:56 +0000

    Fix menu prefabs

--- Unity/Assets/Debugging/Scripts/GameStateDebugMenu.cs
+++ Unity/Assets/Debugging/Scripts/GameStateDebugMenu.cs
@@ -1,80 +1,93 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Reflection;
-//using GameWork.Core.States.Tick;
-//using UnityEngine;
-//using PlayGen.ITAlert.GameStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using GameWork.Core.States.Tick;
+using UnityEngine;
+using PlayGen.ITAlert.Unity.Behaviours.PlayGen.ITAlert.Unity;
+using PlayGen.ITAlert.Unity.GameStates;
+using PlayGen.ITAlert.Unity.GameStates.MenuSubStates;
+using PlayGen.ITAlert.Unity.GameStates.RoomSubStates;
 
-//namespace Debugging
-//{
-//	public class GameStateDebugMenu : MonoBehaviour
-//	{
-//		private ControllerBehaviour _controllerBehaviour;
-//		private TickStateController<TickState> _stateController;
+namespace Debugging
+{
+	public class GameStateDebugMenu : MonoBehaviour
+	{
+		private ControllerBehaviour _controllerBehaviour;
+		private TickStateController<TickState> _stateController;
 
-//		private bool _isVisible = false;
+		private bool _isVisible = false;
 
-//		void Start()
-//		{
-//			_controllerBehaviour = GameObject.FindObjectOfType<ControllerBehaviour>();
+		void Start()
+		{
+			_controllerBehaviour = GameObject.FindObjectOfType<ControllerBehaviour>();
 
-//			_stateController = (TickStateController<TickState>) GetField(_controllerBehaviour.GetType(),
-//				_controllerBehaviour,
-//				"_stateController");
-//		}
+			_stateController = (TickStateController<TickState>)GetField(_controllerBehaviour.GetType(), _controllerBehaviour, "_stateController");
+		}
 
-//		private void Update()
-//		{
-//			if (Input.GetKeyDown(KeyCode.F2))
-//			{
-//				_isVisible = !_isVisible;
-//			}
-//		}
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F2))
+			{
+				_isVisible = !_isVisible;
+			}
+		}
 
-//		private void OnGUI()
-//		{
-//			if (!_isVisible) return;
+		private void OnGUI()
+		{
+			if (!_isVisible) return;
 
-//			GameStateButton(LoginState.StateName);
-//			GameStateButton(LoadingState.StateName);
-//			GameStateButton(MenuState.StateName);
-//			GameStateButton(LobbyState.StateName, MenuState.StateName);
-//			GameStateButton(GamesListState.StateName, MenuState.StateName);
-//			GameStateButton(CreateGameState.StateName, MenuState.StateName);
-//			GameStateButton(SettingsState.StateName, MenuState.StateName);
-//			GameStateButton(RoomState.StateName);
-//		}
+			GameStateButton(LoginState.StateName);
+			GameStateButton(LoadingState.StateName);
+			GameStateButton(MenuState.StateName);
+			GameStateButton(LobbyState.StateName, MenuState.StateName);
+			GameStateButton(GamesListState.StateName, MenuState.StateName);
+			GameStateButton(CreateGameState.StateName, MenuState.StateName);
+			GameStateButton(SettingsState.StateName, MenuState.StateName);
+			GameStateButton(RoomState.StateName);
+		}
 
-//		private object GetField(Type type, object instance, string fieldName)
-//		{
-//			var field = type.GetField(fieldName,
-//				BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-//			var value = field.GetValue(instance);
+		private object GetField(Type type, object instance, string fieldName)
+		{
+			var field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+			var value = field.GetValue(instance);
+			return value;
+		}
 
-//			return value;
-//		}
+		#region Temporary workaround
 
-//		private void GameStateButton(string stateName, string parentState = null)
-//		{
-//			if (GUILayout.Button(stateName))
-//			{
-//				if (parentState != null)
-//				{
-//					_stateController.ChangeState(parentState);
-//					var states = (Dictionary<string, TickState>)GetField(_stateController.GetType(),
-//					_stateController,
-//					"States");
-//					var currentState = states[_stateController.ActiveStateName];
-//					var subStateController = (TickStateController)GetField(currentState.GetType(),
-//					currentState,
-//					"_stateController");
-//					subStateController.ChangeState(stateName);
-//				}
-//				else
-//				{
-//					_stateController.ChangeState(stateName);
-//				}
-//			}
-//		}
-//	}
-//}
+		private void CallChangeStateInternal(object stateController, string stateName)
+		{
+			var changeState = stateController.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic).SingleOrDefault(mi => mi.Name.Equals("ChangeState"));
+			if (changeState != null)
+			{
+				changeState.Invoke(stateController, new object[] { stateName });
+			}
+		}
+
+		#endregion
+
+		private void GameStateButton(string stateName, string parentState = null)
+		{
+			if (GUILayout.Button(stateName))
+			{
+				if (parentState != null)
+				{
+					CallChangeStateInternal(_stateController, parentState);
+					var states = (Dictionary<string, TickState>)GetField(_stateController.GetType(),
+					_stateController,
+					"States");
+					var currentState = states[_stateController.ActiveStateName];
+					var subStateController = (TickStateController)GetField(currentState.GetType(),
+					currentState,
+					"_stateController");
+					CallChangeStateInternal(subStateController, stateName);
+				}
+				else
+				{
+					CallChangeStateInternal(_stateController, stateName);
+				}
+			}
+		}
+	}
+}
