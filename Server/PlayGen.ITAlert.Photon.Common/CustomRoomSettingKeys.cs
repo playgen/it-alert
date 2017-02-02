54a7e35a884af1c92d01fd81e9db13060595f42e
Jared <jaredgglass@gmail.com>
2017-02-02 18:43:42 +0000

    room setting hookup

---
+++ Server/PlayGen.ITAlert.Photon.Common/CustomRoomSettingKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayGen.ITAlert.Photon.Common
+{
+    public static class CustomRoomSettingKeys
+    {
+        public const string MinPlayers = PlayGen.Photon.Common.CustomRoomSettingKeys.MinPlayers;
+
+        public const string CloseOnStarted = "OpenOnEnded";
+
+        public const string OpenOnEnded = "OpenOnEnded";
+    }
+}
