853120f75b118599fdb60b4c587bbc44b662efdf
Jared <jaredgglass@gmail.com>
2017-02-02 18:43:42 +0000

    room setting hookup

---
+++ Server/PlayGen.ITAlert.Photon.Plugin/RoomStates/RoomState.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Hive.Plugin;
+using PlayGen.Photon.Analytics;
+using PlayGen.Photon.Players;
+using PlayGen.Photon.Plugin;
+
+namespace PlayGen.ITAlert.Photon.Plugin.RoomStates
+{
+    public abstract class RoomState : PlayGen.Photon.Plugin.States.RoomState
+    {
+        protected readonly AnalyticsServiceManager Analytics;
+        protected readonly RoomSettings RoomSettings;
+
+        protected RoomState(PluginBase photonPlugin, Messenger messenger, PlayerManager playerManager, RoomSettings roomSettings, AnalyticsServiceManager analytics) 
+            : base(photonPlugin, messenger, playerManager)
+        {
+            RoomSettings = roomSettings;
+            Analytics = analytics;
+        }
+    }
+}
