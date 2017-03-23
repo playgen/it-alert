f0686a7c791ad7bdd9c4d94f567e42ea9d17ab51
Jack Coldridge <jack@playgen.com>
2017-03-23 13:27:05 +0000

    extract graph initialization to systems. differentiate npc and malware entities. rename prefab subdirectory

---
+++ Simulation/PlayGen.ITAlert.Simulation/Systems/Initialization/PlayerSystem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.Entities;
+using Engine.Systems;
+using PlayGen.ITAlert.Simulation.Configuration;
+using PlayGen.ITAlert.Simulation.Exceptions;
+using PlayGen.ITAlert.Simulation.Systems.Movement;
+
+namespace PlayGen.ITAlert.Simulation.Systems.Initialization
+{
+	public class PlayerSystem : IInitializingSystem
+	{
+		private readonly SimulationConfiguration _configuration;
+
+		private readonly IEntityFactoryProvider _entityFactoryProvider;
+		private readonly GraphSystem _graphSystem;
+		private readonly MovementSystem _movementSystem;
+
+
+		public PlayerSystem(SimulationConfiguration configuration, 
+			IEntityFactoryProvider entityFactoryProvider,
+			GraphSystem graphSystem, 
+			MovementSystem movementSystem)
+		{
+			_configuration = configuration;
+			_entityFactoryProvider = entityFactoryProvider;
+			_graphSystem = graphSystem;
+			_movementSystem = movementSystem;
+		}
+
+		#region Implementation of IInitializingSystem
+
+		public void Initialize()
+		{
+			CreatePlayers(_graphSystem.Subsystems, _configuration.PlayerConfiguration);
+		}
+
+		#endregion
+		
+		private void CreatePlayers(Dictionary<int, Entity> subsystems, IEnumerable<PlayerConfig> playerConfigs)
+		{
+			if (playerConfigs.Any())
+			{
+				foreach (var playerConfig in playerConfigs)
+				{
+					if (_entityFactoryProvider.TryCreateEntityFromArchetype(playerConfig.ArchetypeName, out var player))
+					{
+						playerConfig.EntityId = player.Id;
+						var startingLocationId = playerConfig.StartingLocation ?? 0;
+						_movementSystem.AddVisitor(subsystems[startingLocationId], player);
+						continue;
+					}
+					player?.Dispose();
+					throw new SimulationException($"Could not craete player for id '{playerConfig.EntityId}'");
+				}
+			}
+		}
+	}
+}
--- Unity/PlayGen.ITAlert.Unity/PlayGen.ITAlert.Unity/Simulation/Behaviours/NpcBehaviour.cs
+++ Unity/PlayGen.ITAlert.Unity/PlayGen.ITAlert.Unity/Simulation/Behaviours/NpcBehaviour.cs
@@ -1,6 +1,6 @@
-﻿using PlayGen.ITAlert.Simulation.Common;
-using PlayGen.ITAlert.Simulation.Components.Malware;
-using PlayGen.ITAlert.Unity.Exceptions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,112 +8,42 @@ namespace PlayGen.ITAlert.Unity.Simulation.Behaviours
 {
 	public class NpcBehaviour : ActorBehaviour
 	{
-		//private Image _timerImage;
-
-		///private float _imageFillTimer;
-		private bool _pulseDown = true;
-
-		//private string _warningText;
+		public int? InventoryItem => null;
 
 		[SerializeField]
 		private Image _image;
 
 		[SerializeField]
-		private TrailRenderer _trailRenderer;
+		private Canvas _canvas;
 
-		[SerializeField]
-		private Light _light;
-
-		private MalwareVisibility _malwareVisibility;
-		private MalwareGenome _malwareGenome;
+		private Vector3 _scale;
 
 		#region Initialization
-		
+
+		public void Awake()
+		{
+		}
+
 		protected override void OnInitialize()
 		{
 			base.OnInitialize();
-			if (Entity.TryGetComponent(out _malwareVisibility)
-				&& Entity.TryGetComponent(out _malwareGenome))
-			{
-				SetColor();
-			}
-			else
-			{
-				throw new SimulationIntegrationException($"Failed to load NPC component(s) for on entity {Entity.Id}");
-			}
 		}
 
 		#endregion
 
-		#region Unity Update
-
-		protected override void OnFixedUpdate()
+		public void OnEnable()
 		{
+			_scale = ((GameObject)Resources.Load("Player")).GetComponent<RectTransform>().localScale;
+			transform.localScale = new Vector3(_scale.x / transform.parent.localScale.x, _scale.y / transform.parent.localScale.y, 1);
 		}
-
-		protected override void OnUpdate()
-		{
-			var visible = _malwareVisibility.VisibleTo.Contains(Director.Player?.Id ?? -1);
-			if (_image.enabled == false && visible)
-			{
-				_image.enabled = true;
-			}
-			else if (_image.enabled && visible == false)
-			{
-				_image.enabled = false;
-			}
-		}
-
-		private void SetColor()
-		{
-			_image.color = _malwareGenome.GetColourForGenome();
-			_trailRenderer.startColor = _malwareGenome.GetColourForGenome();
-			_trailRenderer.endColor = new Color(_trailRenderer.startColor.r, _trailRenderer.startColor.g, _trailRenderer.startColor.b, 0.375f);
-			_light.color = _malwareGenome.GetColourForGenome();
-		}
-
-		#endregion
 
 		#region State Update
 
 		protected override void OnStateUpdated()
 		{
-			HandlePulse();
 			UpdatePosition();
 		}
 
-		private void HandlePulse()
-		{
-		//TODO: reimplement pulse => visible property
-		//_image.enabled = EntityState.Visible;
-
-		//if (EntityState.Active)
-		//{
-		//	if (_pulseDown)
-		//	{
-		//		_image.color -= new Color(0, 0, 0, 0.05f);
-		//	}
-		//	else
-		//	{
-		//		_image.color += new Color(0, 0, 0, 0.05f);
-		//	}
-		//	if (_image.color.a <= 0)
-		//	{
-		//		_pulseDown = false;
-		//	}
-		//	else if (_image.color.a >= 1)
-		//	{
-		//		_pulseDown = true;
-		//	}
-		//}
-		//else
-		//{
-		//	_pulseDown = false;
-		//	_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1);
-		//}
-		}
-
 		#endregion
-
 	}
 }
