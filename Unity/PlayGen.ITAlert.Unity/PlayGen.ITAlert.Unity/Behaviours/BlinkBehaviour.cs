1073b032deb741b179ca16a75ecaa9f5858cce79
Jack Coldridge <jack@playgen.com>
2017-04-04 12:07:50 +0100

    sprite updates

--- Unity/PlayGen.ITAlert.Unity/PlayGen.ITAlert.Unity/Behaviours/BlinkBehaviour.cs
+++ Unity/PlayGen.ITAlert.Unity/PlayGen.ITAlert.Unity/Behaviours/BlinkBehaviour.cs
@@ -16,59 +16,34 @@ namespace PlayGen.ITAlert.Unity.Behaviours
 		[SerializeField]
 		private float _interval = 1f;
 
-		[SerializeField]
-		private bool _periodic = true;
-
-		private bool _pulseDown;
+		private float _progress;
 
 		private float _initialAlpha;
 
 		[SerializeField]
 		private AnimationCurve _curve;
 
-		public void AWake()
+		public void Awake()
 		{
+			_initialAlpha = _image.color.a;
 		}
 
 		public void Update()
 		{
-			Pulse(Time.deltaTime / _interval / 2);
+			_progress += Time.deltaTime;
+			_progress %= _interval;
+
+			_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, _curve.Evaluate(_progress / _interval) * _initialAlpha);
 		}
 
 		public void OnEnable()
 		{
-			_pulseDown = true;
-			_initialAlpha = _image.color.a;
+			_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, _initialAlpha);
 		}
 
 		public void OnDisable()
 		{
 			_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, _initialAlpha);
 		}
-
-		public void Pulse(float step)
-		{
-			if (_pulseDown)
-			{
-				_image.color -= new Color(0, 0, 0, step);
-			}
-			else
-			{
-				_image.color += new Color(0, 0, 0, step);
-			}
-			if (_image.color.a <= 0)
-			{
-				_pulseDown = false;
-			}
-			else if (_image.color.a >= _initialAlpha)
-			{
-				_pulseDown = true;
-				if (_periodic == false)
-				{
-					this.enabled = false;
-				}
-			}
-		}
-
 	}
 }
