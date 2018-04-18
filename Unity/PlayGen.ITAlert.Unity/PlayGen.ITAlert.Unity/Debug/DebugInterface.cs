2223feb6f2d5fb28cbcba45f1d2b88eb69e61e48
Ellis Spice <ellis@playgen.com>
2018-04-18 12:07:05 +0100

    Fix for Unity debug files not being committed

---
+++ Unity/PlayGen.ITAlert.Unity/PlayGen.ITAlert.Unity/Debug/DebugInterface.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace PlayGen.ITAlert.Unity.Debug
+{
+	public class DebugInterface : MonoBehaviour
+	{
+		private bool _showInterface;
+
+		private void Update()
+		{
+			if (Application.isEditor)
+			{
+				if (Input.GetKeyDown(KeyCode.F5))
+				{
+					_showInterface = !_showInterface;
+				}
+			}
+		}
+
+		private void OnGUI()
+		{
+			if (_showInterface)
+			{
+				if (GUI.Button(new Rect(0, 0, 150, 20), "Increase Speed"))
+				{
+					DebugCommands.ChangePlayerSpeedOffset(0.1m);
+				}
+				if (GUI.Button(new Rect(0, 25, 150, 20), "Decrease Speed"))
+				{
+					DebugCommands.ChangePlayerSpeedOffset(-0.1m);
+				}
+			}
+		}
+	}
+}
